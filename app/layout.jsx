315ad6bd5b0e1d151fd9a0a104b29bedf985ffe6
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-25 15:23:45 -0700

    Add Layout and Homepage changes

---
+++ app/layout.jsx
@@ -0,0 +1,11 @@
+import '@/assets/styles/global.css'
+
+const MainLayout = ({children}) => {
+    return ( <html>
+        <body>
+            <main>{children}</main>
+        </body>
+    </html> );
+}
+ 
+export default MainLayout;
