c3628044a19cc6ab75e18f1b91f720fda1d779d6
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-02-25 15:55:39 -0700

    Add Metadata and Assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,10 @@
-import '@/assets/styles/global.css'
+import '@/assets/styles/global.css';
+
+export const metadata ={
+    title: 'Property Pulse',
+    keywords: 'rental, property, real estate',
+    description: 'Find the perfect rental property',
+};
 
 const MainLayout = ({children}) => {
     return ( <html>
