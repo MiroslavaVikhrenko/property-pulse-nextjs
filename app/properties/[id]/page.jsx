ee60323033fa6fe95269da1cbbc9791211c65a28
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-01 17:39:31 -0700

    Update PropertyPage to fetch data from mongoDB

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,10 @@
-const PropertyPage = ({ params }) => {
-    return <div>Property Page {params.id} </div>;
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+const PropertyPage = async ({ params }) => {
+    await connectDB();
+    const property = await Property.findById(params.id).lean();
+    return <section>{property.name}</section>;
 };
  
 export default PropertyPage;
