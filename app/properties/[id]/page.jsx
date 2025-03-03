cd2d50bc4b0ff0342466786cef14c4bfdc5343dc
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-03 13:48:24 -0700

    Update PropertyPage

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -20,7 +20,13 @@ const PropertyPage = async ({ params }) => {
         </Link>
         </div>
         </section>
-        <section>{property.name}</section>
+        <section className="bg-blue-50">
+            <div className="container m-auto py-10 px-6">
+                <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                    {/* Property Info */}
+                </div>
+            </div>
+        </section>
         </>
 );
 };
