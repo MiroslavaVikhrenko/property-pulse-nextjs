5ef7d56fa1736084aff9383116aff683ae4a4131
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-13 17:40:49 -0600

    Add Cloudinary as a source (= as a remote pattern) in next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,16 @@ const nextConfig = {
     images: {
         // array of objects of remote domains that we want to get images from
         remotePatterns: [
+            // Add Google as a source (= as a remote pattern)
             {
                 protocol: 'https',
                 hostname: 'lh3.googleusercontent.com', // google account image comes from here
+                pathname: '**'
+            },
+            // Add Cloudinary as a source (= as a remote pattern)
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com', // cloudinary domain where the images comes from
                 pathname: '**'
             },
         ],
