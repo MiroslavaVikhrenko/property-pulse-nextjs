144ab46b1a5c61fafc3e9661bf1f77f107242512
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-13 15:53:26 -0600

    Properly format data from PropertyAddForm in addProperty action

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,7 +1,49 @@
 'use server';
 
 async function addProperty(formData) {
-    console.log(formData.getAll('amenities'))
+    // Access all values from amenities and images => we use exact field names from PropertyAddForm component
+    const amenities = formData.getAll('amenities')
+    // Images are different as getAll() would give us image objects, not just the name
+    // But we want juat an array of image names
+    // So we can use combination of filter() and map() for that
+    // filter() => filter out any empty names 
+    // map() => re-format it to just an array of image names
+
+    //ultimately these images are going to come from Cloudinary 
+    const images = formData
+        .getAll('images')
+        .filter((image) => image.name !== '')
+        .map((image) => image.name);
+    //console.log(images);
+
+    const propertyData = {
+        // get() as it's a single value
+        type: formData.get('type'),
+        name: formData.get('name'),
+        description: formData.get('description'),
+        location: {
+            street: formData.get('location.street'),
+            city: formData.get('location.city'),
+            state: formData.get('location.state'),
+            zipcode: formData.get('location.zipcode'),
+        },
+        beds: formData.get('beds'),
+        baths: formData.get('baths'),
+        square_feet: formData.get('square_feet'),
+        amenities,
+        rates: {
+            nightly: formData.get('rates.nightly'),
+            weekly: formData.get('rates.weekly'),
+            monthly: formData.get('rates.monthly')
+        },
+        seller_info: {
+            name: formData.get('seller_info.name'),
+            email: formData.get('seller_info.email'),
+            phone: formData.get('seller_info.phone'),
+        },
+        images
+    };
+    console.log(propertyData);
 }
 
 export default addProperty;
