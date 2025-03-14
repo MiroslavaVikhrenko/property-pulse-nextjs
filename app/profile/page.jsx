46925c8b12e5c788ca791e7f9c68c74c35d34ff7
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-14 15:37:36 -0600

    Update ProfilePage to display real user info

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,17 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser"; // to get user
 import profileDefault from '@/assets/images/profile.png'; // default image in case needed
 
-const ProfilePage = () => {
+const ProfilePage = async () => {
+    // Connect to db
+    await connectDB();
+    // Get session
+    const sessionUser = await getSessionUser();
+    // Destructure user id from that
+    const {userId} = sessionUser;
+    // If there is no user id
+    if (!userId) {
+        throw new Error('User ID is required'); // show error.jsx page
+    }
     return (
         <section className="bg-blue-50">
       <div className="container m-auto py-24">
@@ -15,18 +25,20 @@ const ProfilePage = () => {
           <div className="flex flex-col md:flex-row">
             <div className="md:w-1/4 mx-20 mt-10">
               <div className="mb-4">
-                <img
+                <Image
                   className="h-32 w-32 md:h-48 md:w-48 rounded-full mx-auto md:mx-0"
-                  src="/images/profile.png"
+                  src={sessionUser.user.image || profileDefault}
+                  width={200}
+                  height={200}
                   alt="User"
                 />
               </div>
 
               <h2 className="text-2xl mb-4">
-                <span className="font-bold block">Name: </span> John Doe
+                <span className="font-bold block">Name: </span> {sessionUser.user.name}
               </h2>
               <h2 className="text-2xl">
-                <span className="font-bold block">Email: </span> john@gmail.com
+                <span className="font-bold block">Email: </span> {sessionUser.user.email}
               </h2>
             </div>
 
