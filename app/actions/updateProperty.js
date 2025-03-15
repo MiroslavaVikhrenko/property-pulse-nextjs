a96cc1b00a1422792661d917cf65dba37dc1e369
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-14 18:49:07 -0600

    Create updateProperty server action

---
+++ app/actions/updateProperty.js
@@ -0,0 +1,7 @@
+'use server';
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from 'next/navigation';
+
