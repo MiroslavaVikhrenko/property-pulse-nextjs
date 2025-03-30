719cb25a54dbf06149115351f2d433f8603ef740
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-30 14:41:42 -0600

    Bring MessageCard component to MessagesPage

--- app/messages/page.jsx
+++ app/messages/page.jsx
@@ -3,6 +3,7 @@ import Message from "@/models/Message";
 import '@/models/Property'; // Needed as we're going to use populate() method where we populate properties
 import { convertToSerializableObject } from "@/utils/convertToObject";
 import { getSessionUser } from "@/utils/getSessionUser";
+import MessageCard from "@/components/MessageCard";
 
 const MessagesPage = async () => {
     connectDB();
@@ -47,7 +48,7 @@ const MessagesPage = async () => {
                 <div className="space-y-4">
                     {messages.length === 0 ? (<p>You have no messages</p>) : (
                         messages.map((message) => (
-                            <h3 key={message._id}>{message.name}</h3>
+                            <MessageCard key={message._id} message={message} />
                         ))
                     )}
                 </div>
