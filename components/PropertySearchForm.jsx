aa57161aa17f154d6ad5fcb59bc71997a76f494a
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-29 19:38:32 -0600

    Update PropertySearchForm component

--- components/PropertySearchForm.jsx
+++ components/PropertySearchForm.jsx
@@ -1,6 +1,33 @@
+'use client';
+import { useState } from "react";
+import { useRouter } from "next/navigation"; // for redirect
+
 const PropertySearchForm = () => {
+    // Bring state 
+    const [location, setLocation] = useState(''); // default - empty string
+    const [propertyType, setPropertyType] = useState('All'); // default - All
+
+    // Initialize the router
+    const router = useRouter();
+
+    const handleSubmit = (e) => {
+        // Prevent default behavior
+        e.preventDefault();
+
+        // Check for the location (input) => if it's submitted and nothing is filled out => go to the properties page
+        if (location === '' && propertyType === 'All') {
+            // Redirect
+            router.push('/properties');
+        } else {
+            const query = `?location=${location}&propertyType=${propertyType}`;
+            // Redirect
+            router.push(`/properties/search-results${query}`);
+        }
+    }
+
     return ( 
         <form
+            onSubmit={handleSubmit}
             className="mt-3 mx-auto max-w-2xl w-full flex flex-col md:flex-row items-center"
           >
             <div className="w-full md:w-3/5 md:pr-2 mb-4 md:mb-0">
@@ -10,6 +37,8 @@ const PropertySearchForm = () => {
                 id="location"
                 placeholder="Enter Location (City, State, Zip, etc"
                 className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
               />
             </div>
             <div className="w-full md:w-2/5 md:pl-2">
@@ -17,6 +46,8 @@ const PropertySearchForm = () => {
               <select
                 id="property-type"
                 className="w-full px-4 py-3 rounded-lg bg-white text-gray-800 focus:outline-none focus:ring focus:ring-blue-500"
+                value={propertyType}
+                onChange={(e) => setPropertyType(e.target.value)}
               >
                 <option value="All">All</option>
                 <option value="Apartment">Apartment</option>
