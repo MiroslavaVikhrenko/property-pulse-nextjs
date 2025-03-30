d67de26dd67aad423f73c001b7402f77332ada4d
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-30 16:37:27 -0600

    Create GlobalContext.js for updating new message count across the whole app using createContext, useContext, useState

---
+++ context/GlobalContext.js
@@ -0,0 +1,34 @@
+'use client';
+// as we need connection between navbar and message card components for updating count of new messages
+// we'll use context for it
+// Context is a way to share state between components and doing so without passing it into a component by props
+// we can alternatively use Redux package, but react has context API that we will use
+import { createContext, useContext, useState } from "react"; 
+
+// Create Context
+const GlobalContext = createContext();
+
+// Create Provider
+// Context needs a provider and you wrap your app in this provider in order to have access to that context,
+// so that every component in your app can get access to it
+export function GlobalProvider({children}) {
+    // This will be sent down to every component
+    // The only state that we want is the unread count - 0 by default
+    const {unreadCount, setUnreadCount} = useState(0);
+
+    // Pass setUnreadCount so that the count can be changed within all components
+    // wrapping {children} => which ultimately will be our entire app and all the components
+    return (
+        <GlobalContext.Provider value={{
+            unreadCount,
+            setUnreadCount
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+}
+
+// Create custom hook to access that context => pass in our global context
+export function useGlobalContext() {
+    return useContext(GlobalContext);
+}
