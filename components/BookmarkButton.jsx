a28cc0db0689573571c438e13e64aa66f08a104d
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-29 18:03:03 -0600

    Complete check Bookmark Status functionality on BookmarkButton component

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react'; 
 // when component loads we want to check the status
 import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import checkBookmarkStatus from '@/app/actions/checkBookmarkStatus';
 import { toast } from "react-toastify"; // to use toast notifications
 import { FaBookmark } from "react-icons/fa";
 import { useSession } from "next-auth/react";  // useSession hook
@@ -15,6 +16,24 @@ const BookmarkButton = ({property}) => {
     // Add states
     const [isBookmarked, setIsBookmarked] = useState(false);
     const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Return if not logged in
+        if (!userId) {
+            setLoading(false);
+            return;
+        }
+
+        // Call checkBookmarkStatus
+        checkBookmarkStatus(property._id).then((res) => {
+            // Check for error
+            if(res.error) toast.error(res.error);
+            // Check for isBookmarked
+            if(res.isBookmarked) setIsBookmarked(res.isBookmarked);
+            // Set loading to false
+            setLoading(false);
+        });
+    }, [property._id, userId, checkBookmarkStatus]);
 
     // If not logged in, they can't bookmark property
     const handleClick = async () => {
@@ -33,6 +52,11 @@ const BookmarkButton = ({property}) => {
             toast.success(res.message); // message from action
         })
     };
+
+    // Check to see if it's loading
+    if(loading) {
+        return <p className="text-center">Loading...</p>
+    }
 
     return isBookmarked ? (
         <button className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
