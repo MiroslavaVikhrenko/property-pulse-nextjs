66c103bc23211905b0ef1d6f2f4614e39701c170
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-29 21:44:43 -0600

    Move SubmitMessageButton from PropertyContactForm component to a separate component

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -111,12 +111,7 @@ const PropertyContactForm = ({property}) => {
                   ></textarea>
                 </div>
                 <div>
-                  <button
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
-                    type="submit"
-                  >
-                    <FaPaperPlane className='mr-2' /> Send Message
-                  </button>
+                  
                 </div>
               </form>
             </div>
