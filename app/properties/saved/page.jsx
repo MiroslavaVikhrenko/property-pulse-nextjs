6a1dca575e4dfee47f8c18aa978f776fa7aaaa00
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-03-29 18:32:25 -0600

    Complete SavedPropertiesPage

--- app/properties/saved/page.jsx
+++ app/properties/saved/page.jsx
@@ -1,5 +1,29 @@
-const SavedPropertiesPage = () => {
-    return <div>Saved</div>;
+import PropertyCard from "@/components/PropertyCard";
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+const SavedPropertiesPage = async () => {
+    // Get the Id from session user
+    const {userId} = await getSessionUser();
+
+    // Get bookmarks by de-structuring the user - user ( User.findById(userId) ) will give bookmarks array which has the property IDs 
+    // but we want the whole properties
+    // populate('bookmarks') => we're populating that bookmarks array with the actual properties
+    const {bookmarks} = await User.findById(userId).populate('bookmarks');
+
+    return <section className='px-4 py-6'>
+        <div className="container lg:container m-auto px-4 py-6">
+            <h1 className="text-2xl mb-4">Saved Properties</h1>
+            {bookmarks.length === 0 ? (<p>No saved properties</p>) : (
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    {bookmarks.map((property) =>(
+                        <PropertyCard key={property._id} property={property} />
+                    ))}
+                </div>
+            )}
+        </div>
+    </section>;
 };
  
 export default SavedPropertiesPage;
