b9ed349c22fd78fa18fc664fd0826ae8f52e7a89
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-01 17:07:41 -0600

    Add searchParams to PropertiesPage and set limit to showing only 2 properties per page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -2,11 +2,17 @@ import PropertyCard from '@/components/PropertyCard';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 
-const PropertiesPage = async () => {
+// searchParams for pagination => to be able to go to /properties?page=1 or 2, 3...
+// De-structure searchParams and set default 1 for page
+const PropertiesPage = async ({searchParams: {page = 1, pageSize = 2}}) => {
     await connectDB();
+
+    const skip = (page - 1) * pageSize;
+
+    const total = await Property.countDocuments({});
     // find({}) - passing empty object as not specifying filter, lean() - optimize query performance
     // by returning plain JS objects instead of mongoose documents (read only)
-    const properties = await Property.find({}).lean();
+    const properties = await Property.find({}).skip(skip).limit(pageSize);
     return (
         <section className='px-4 py-6'>
             <div className='container-xl lg:container m-auto px-4 py-6'>
