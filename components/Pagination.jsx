8943eba2e8f03bb0919bf83798e338149c52f50b
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-01 17:23:55 -0600

    Update markup for Pagination component

--- components/Pagination.jsx
+++ components/Pagination.jsx
@@ -1,5 +1,16 @@
-const Pagination = () => {
-    return <div>Pagination</div>;
+// Component to switch between properties pages 
+const Pagination = ({page, pageSize, totalItems}) => {
+    return <section className='container mx-auto flex justify-center items-center my-8'>
+        <a href="#" className="mr-2 px-2 py-1 border border-gray-300 rounded">
+            Previous
+        </a>
+
+        <span className="mx-2">Page 1 of 4</span>
+
+        <a href="#" className="ml-2 px-2 py-1 border border-gray-300 rounded">
+            Next
+        </a>
+    </section>;
 };
  
 export default Pagination;
